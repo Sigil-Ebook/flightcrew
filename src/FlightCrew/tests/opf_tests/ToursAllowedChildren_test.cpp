487b1c691be2a6da37cf77afff5b03716d544140
Strahinja Markovic <devnull@localhost>
2010-07-29 21:24:46 +0200

    Adding a check for allowed <tours> children

---
+++ src/FlightCrew/tests/opf_tests/ToursAllowedChildren_test.cpp
@@ -0,0 +1,45 @@
+/************************************************************************
+**
+**  Copyright (C) 2010  Strahinja Markovic
+**
+**  This file is part of FlightCrew.
+**
+**  FlightCrew is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU Lesser General Public License as published
+**  by the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  FlightCrew is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU Lesser General Public License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public License
+**  along with FlightCrew.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include <stdafx_tests.h>
+#include "Validators/Opf/ToursAllowedChildren.h"
+#include "Result.h"
+
+TEST( ToursAllowedChildrenTest, WrongChildren )
+{
+    ToursAllowedChildren validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/ToursAllowedChildren_WrongChildren.xml" );
+
+    EXPECT_EQ( results[ 0 ].GetErrorId(), ERROR_XML_CHILD_NOT_RECOGNIZED );
+    EXPECT_EQ( results[ 0 ].GetErrorLine(), 4 );
+    EXPECT_EQ( results[ 0 ].GetErrorColumn(), 17 );
+}
+
+TEST( ToursAllowedChildrenTest, ChildrenOK )
+{
+    ToursAllowedChildren validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/ToursAllowedChildren_ChildrenOK.xml" );
+
+    EXPECT_EQ( results[ 0 ].GetErrorId(), ALL_OK );
+}
+
