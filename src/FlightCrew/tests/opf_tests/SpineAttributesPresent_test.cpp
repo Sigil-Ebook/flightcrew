15ed0ac301757bccde4745483b84690d7a6c7515
Strahinja Markovic <devnull@localhost>
2010-08-02 22:36:33 +0200

    Adding a check for <spine> allowed and mandatory attributes

---
+++ src/FlightCrew/tests/opf_tests/SpineAttributesPresent_test.cpp
@@ -0,0 +1,68 @@
+/************************************************************************
+**
+**  Copyright (C) 2010  Strahinja Markovic
+**
+**  This file is part of FlightCrew.
+**
+**  FlightCrew is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU Lesser General Public License as published
+**  by the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  FlightCrew is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU Lesser General Public License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public License
+**  along with FlightCrew.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include <stdafx_tests.h>
+#include "Validators/Opf/SpineAttributesPresent.h"
+#include "Result.h"
+
+using namespace FlightCrew;
+
+TEST( SpineAttributesPresentTest, NotAllowedAttributes )
+{
+    SpineAttributesPresent validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/SpineAttributesPresent_NotAllowedAttributes.xml" );
+            
+    EXPECT_EQ( results.size(), 1U );
+    EXPECT_EQ( results[ 0 ].GetResultId(), ERROR_XML_ATTRIBUTE_NOT_RECOGNIZED );
+    EXPECT_EQ( results[ 0 ].GetErrorLine(), 3 );
+    EXPECT_EQ( results[ 0 ].GetErrorColumn(), 51 );
+    
+    std::vector< std::string > message_arguments1 = results[ 0 ].GetMessageArguments();
+    EXPECT_EQ( message_arguments1[ 0 ], "breakme" );
+    EXPECT_EQ( message_arguments1[ 1 ], "spine" );
+}
+
+TEST( SpineAttributesPresentTest, MissingAttributes )
+{
+    SpineAttributesPresent validator;
+    std::vector<Result> results = validator.ValidateFile(
+        "test_data/opf_tests/SpineAttributesPresent_MissingAttributes.xml" );
+
+    EXPECT_EQ( results.size(), 1U );
+    EXPECT_EQ( results[ 0 ].GetResultId(), ERROR_XML_REQUIRED_ATTRIBUTE_MISSING );
+    EXPECT_EQ( results[ 0 ].GetErrorLine(), 3 );
+    EXPECT_EQ( results[ 0 ].GetErrorColumn(), 24 );
+
+    std::vector< std::string > message_arguments = results[ 0 ].GetMessageArguments();
+    EXPECT_EQ( message_arguments[ 0 ], "toc" );
+    EXPECT_EQ( message_arguments[ 1 ], "spine" );
+}
+
+TEST( SpineAttributesPresentTest, AllowedAttributes )
+{
+    SpineAttributesPresent validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/SpineAttributesPresent_AllowedAttributes.xml" );
+    
+    EXPECT_EQ( results.size(), 0U );
+}
+
