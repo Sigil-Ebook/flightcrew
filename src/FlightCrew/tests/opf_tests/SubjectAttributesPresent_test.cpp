f5ed1bcd7d7883639f86598d406e61d9032ab8cd
Strahinja Markovic <devnull@localhost>
2010-08-02 20:21:03 +0200

    Adding a check for <subject> allowed and mandatory attributes

---
+++ src/FlightCrew/tests/opf_tests/SubjectAttributesPresent_test.cpp
@@ -0,0 +1,52 @@
+/************************************************************************
+**
+**  Copyright (C) 2010  Strahinja Markovic
+**
+**  This file is part of FlightCrew.
+**
+**  FlightCrew is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU Lesser General Public License as published
+**  by the Free Software Foundation, either version 3 of the License, or
+**  (at your option) any later version.
+**
+**  FlightCrew is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU Lesser General Public License for more details.
+**
+**  You should have received a copy of the GNU Lesser General Public License
+**  along with FlightCrew.  If not, see <http://www.gnu.org/licenses/>.
+**
+*************************************************************************/
+
+#include <stdafx_tests.h>
+#include "Validators/Opf/SubjectAttributesPresent.h"
+#include "Result.h"
+
+using namespace FlightCrew;
+
+TEST( SubjectAttributesPresentTest, NotAllowedAttributes )
+{
+    SubjectAttributesPresent validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/SubjectAttributesPresent_NotAllowedAttributes.xml" );
+            
+    EXPECT_EQ( results.size(), 1U );
+    EXPECT_EQ( results[ 0 ].GetResultId(), ERROR_XML_ATTRIBUTE_NOT_RECOGNIZED );
+    EXPECT_EQ( results[ 0 ].GetErrorLine(), 4 );
+    EXPECT_EQ( results[ 0 ].GetErrorColumn(), 67 );
+    
+    std::vector< std::string > message_arguments1 = results[ 0 ].GetMessageArguments();
+    EXPECT_EQ( message_arguments1[ 0 ], "breakme" );
+    EXPECT_EQ( message_arguments1[ 1 ], "subject" );
+}
+
+TEST( SubjectAttributesPresentTest, AllowedAttributes )
+{
+    SubjectAttributesPresent validator;
+    std::vector<Result> results = validator.ValidateFile(
+            "test_data/opf_tests/SubjectAttributesPresent_AllowedAttributes.xml" );
+    
+    EXPECT_EQ( results.size(), 0U );
+}
+
