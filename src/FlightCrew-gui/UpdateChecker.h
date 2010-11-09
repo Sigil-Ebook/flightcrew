41a1970e377a25e22c76ec7b7ef9e638310c10ae
Strahinja Markovic <devnull@localhost>
2010-11-09 19:14:08 +0100

    Updated the copyright notices in the update checker sources

--- src/FlightCrew-gui/UpdateChecker.h
+++ src/FlightCrew-gui/UpdateChecker.h
@@ -1,21 +1,21 @@
 /************************************************************************
 **
-**  Copyright (C) 2009, 2010  Strahinja Markovic
+**  Copyright (C) 2010  Strahinja Markovic
 **
-**  This file is part of Sigil.
+**  This file is part of FlightCrew.
 **
-**  Sigil is free software: you can redistribute it and/or modify
-**  it under the terms of the GNU General Public License as published by
-**  the Free Software Foundation, either version 3 of the License, or
+**  FlightCrew is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU Lesser General Public License as published
+**  by the Free Software Foundation, either version 3 of the License, or
 **  (at your option) any later version.
 **
-**  Sigil is distributed in the hope that it will be useful,
+**  FlightCrew is distributed in the hope that it will be useful,
 **  but WITHOUT ANY WARRANTY; without even the implied warranty of
 **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-**  GNU General Public License for more details.
+**  GNU Lesser General Public License for more details.
 **
-**  You should have received a copy of the GNU General Public License
-**  along with Sigil.  If not, see <http://www.gnu.org/licenses/>.
+**  You should have received a copy of the GNU Lesser General Public License
+**  along with FlightCrew.  If not, see <http://www.gnu.org/licenses/>.
 **
 *************************************************************************/
 
