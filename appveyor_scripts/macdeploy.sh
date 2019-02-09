debc06bbc815bae737e441b4e60474c053e2663e
Doug Massay <douglaslmassay@gmail.com>
2019-02-09 10:25:16 -0500

    Initial gdrive deployment test for Mac on Travis

---
+++ appveyor_scripts/macdeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#ARCHIVE=$REPO_NAME-$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$COMMIT.tar.bz2
+#echo "Creating archive $ARCHIVE"
+#tar cfj $ARCHIVE dist
+#FILESIZE=$(stat -c%s "$ARCHIVE")
+#echo "Finished archive (size $FILESIZE), starting Google Drive upload"
+#./bin/gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
+echo "Testing gdrive"
+./bin/gdrive list
+echo "Finished Google Drive upload"
