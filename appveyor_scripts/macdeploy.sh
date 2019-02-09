fa2a3deb7bade60a1eeca061967b0df3861acdc6
Doug Massay <douglaslmassay@gmail.com>
2019-02-09 16:25:44 -0500

    Test Travis Mac gdrive deploy

--- appveyor_scripts/macdeploy.sh
+++ appveyor_scripts/macdeploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-ORIGFILENAME=./bin/flightcrew-plugin
-NEWFILENAME="$ORIGFILENAME-$TRAVIS_BRANCH-$TRAVIS_COMMIT-$(date +%Y-%m-%d_%H_%M_%S)"
-cp "$ORIGFILENAME" "$NEWFILENAME"
-FILESIZE=$(stat -f%z "$NEWFILENAME")
+ORIGFILENAME=flightcrew-plugin
+COMMIT=$(echo $TRAVIS_COMMIT | cut -c1-7)
+DTE=$(echo $(date +%Y-%m-%d_%H_%M_%S) | cut -c1-10)
+NEWFILENAME="$ORIGFILENAME-$TRAVIS_BRANCH-$COMMIT-$DTE-mac"
+cp "./bin/$ORIGFILENAME" "./bin/$NEWFILENAME"
+FILESIZE=$(stat -f%z "./bin/$NEWFILENAME")
 echo "Finished rename (size $FILESIZE), starting Google Drive upload"
-gdrive --refresh-token $GDRIVE_REFRESH_TOKEN upload --parent $GDRIVE_DIR "$NEWFILENAME"
+gdrive --refresh-token $GDRIVE_REFRESH_TOKEN upload --parent $GDRIVE_DIR "./bin/$NEWFILENAME"
 echo "Finished Google Drive upload"
