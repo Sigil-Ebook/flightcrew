29c5d2d35a4cbb6e287681317a572ca5558aaf06
Doug Massay <douglaslmassay@gmail.com>
2019-02-09 14:35:00 -0500

    Travis Test #18 [skip appveyor]

--- appveyor_scripts/macdeploy.sh
+++ appveyor_scripts/macdeploy.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
-
-#ARCHIVE=$REPO_NAME-$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$COMMIT.tar.bz2
-#echo "Creating archive $ARCHIVE"
-#tar cfj $ARCHIVE dist
-#FILESIZE=$(stat -c%s "$ARCHIVE")
-#echo "Finished archive (size $FILESIZE), starting Google Drive upload"
-#./bin/gdrive upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
-echo "Testing gdrive"
-echo "Refresh token $GDRIVE_REFRESH_TOKEN"
-gdrive --refresh-token $GDRIVE_REFRESH_TOKEN list
-FILESIZE=$(stat -f%z ./bin/flightcrew-plugin)
-echo "File size = $FILESIZE"
-pwd
-gdrive --refresh-token $GDRIVE_REFRESH_TOKEN upload --parent $GDRIVE_DIR ./bin/flightcrew-plugin
+ORIGFILENAME=./bin/flightcrew-plugin
+NEWFILENAME="$ORIGFILENAME-$TRAVIS_BRANCH-$TRAVIS_COMMIT-$(date +%Y-%m-%d_%H_%M_%S)"
+cp "$ORIGFILENAME" "$NEWFILENAME"
+FILESIZE=$(stat -f%z "$NEWFILENAME")
+echo "Finished rename (size $FILESIZE), starting Google Drive upload"
+gdrive --refresh-token $GDRIVE_REFRESH_TOKEN upload --parent $GDRIVE_DIR "$NEWFILENAME"
 echo "Finished Google Drive upload"
